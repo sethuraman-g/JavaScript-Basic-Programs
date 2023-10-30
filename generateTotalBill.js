cccfb06cd4dcc589a68a27d27a8c132f46534441
sethuraman-g <148437573+sethuraman-g@users.noreply.github.com>
2023-10-30 15:52:00 +0530

    #DAY 7 JS

---
+++ generateTotalBill.js
@@ -0,0 +1,144 @@
+
+let storeList = [{"sku":1,"price":"$94.11","product":"Jam - Apricot","Quantity":2},
+{"sku":2,"price":"$13.47","product":"Sauce - Sesame Thai Dressing","Quantity":17},
+{"sku":3,"price":"$49.54","product":"Creamers - 10%","Quantity":17},
+{"sku":4,"price":"$6.31","product":"Compound - Passion Fruit","Quantity":24},
+{"sku":5,"price":"$76.19","product":"Chip - Potato Dill Pickle","Quantity":15},
+{"sku":6,"price":"$96.57","product":"Cake - Box Window 10x10x2.5","Quantity":25},
+{"sku":7,"price":"$74.49","product":"Beets - Candy Cane, Organic","Quantity":18},
+{"sku":8,"price":"$57.09","product":"Vinegar - Balsamic","Quantity":12},
+{"sku":9,"price":"$11.25","product":"Ice Cream Bar - Oreo Sandwich","Quantity":20},
+{"sku":10,"price":"$2.66","product":"Yams","Quantity":21},
+{"sku":11,"price":"$48.37","product":"Chinese Foods - Pepper Beef","Quantity":12},
+{"sku":12,"price":"$91.00","product":"Bread - Sour Sticks With Onion","Quantity":16},
+{"sku":13,"price":"$88.27","product":"Split Peas - Green, Dry","Quantity":11},
+{"sku":14,"price":"$83.29","product":"Leeks - Large","Quantity":17},
+{"sku":15,"price":"$92.52","product":"Allspice - Jamaican","Quantity":17},
+{"sku":16,"price":"$11.91","product":"Wine - Red, Mouton Cadet","Quantity":21},
+{"sku":17,"price":"$69.92","product":"Daves Island Stinger","Quantity":24},
+{"sku":18,"price":"$92.33","product":"Nut - Pine Nuts, Whole","Quantity":19},
+{"sku":19,"price":"$72.55","product":"Swordfish Loin Portions","Quantity":20},
+{"sku":20,"price":"$40.48","product":"Salt And Pepper Mix - Black","Quantity":16},
+{"sku":21,"price":"$24.76","product":"Eggplant Italian","Quantity":19},
+{"sku":22,"price":"$19.06","product":"Goulash Seasoning","Quantity":17},
+{"sku":23,"price":"$8.89","product":"Seabream Whole Farmed","Quantity":14},
+{"sku":24,"price":"$28.58","product":"Ice Cream - Life Savers","Quantity":18},
+{"sku":25,"price":"$43.30","product":"Teriyaki Sauce","Quantity":25},
+{"sku":26,"price":"$54.39","product":"Juice - Orangina","Quantity":24},
+{"sku":27,"price":"$33.78","product":"Lettuce - Romaine","Quantity":25},
+{"sku":28,"price":"$95.64","product":"Appetizer - Mini Egg Roll, Shrimp","Quantity":24},
+{"sku":29,"price":"$26.63","product":"Tart - Raisin And Pecan","Quantity":24},
+{"sku":30,"price":"$13.98","product":"Wine - Zonnebloem Pinotage","Quantity":16},
+{"sku":31,"price":"$77.81","product":"Liquid Aminios Acid - Braggs","Quantity":20},
+{"sku":32,"price":"$92.53","product":"Wine - Pinot Noir Stoneleigh","Quantity":9},
+{"sku":33,"price":"$51.27","product":"Pork - Suckling Pig","Quantity":8},
+{"sku":34,"price":"$57.16","product":"Wine - Saint Emilion Calvet","Quantity":4},
+{"sku":35,"price":"$45.07","product":"Squeeze Bottle","Quantity":23},
+{"sku":36,"price":"$53.40","product":"Tarragon - Fresh","Quantity":17},
+{"sku":37,"price":"$34.66","product":"Oil - Macadamia","Quantity":20},
+{"sku":38,"price":"$62.82","product":"Vegetable - Base","Quantity":1},
+{"sku":39,"price":"$10.76","product":"Wine - Chenin Blanc K.w.v.","Quantity":14},
+{"sku":40,"price":"$77.96","product":"Rappini - Andy Boy","Quantity":13},
+{"sku":41,"price":"$95.10","product":"Cup - 8oz Coffee Perforated","Quantity":1},
+{"sku":42,"price":"$11.34","product":"Crab - Meat","Quantity":18},
+{"sku":43,"price":"$61.35","product":"Greens Mustard","Quantity":2},
+{"sku":44,"price":"$60.45","product":"Chevril","Quantity":10},
+{"sku":45,"price":"$13.40","product":"Soupcontfoam16oz 116con","Quantity":22},
+{"sku":46,"price":"$4.67","product":"Spice - Onion Powder Granulated","Quantity":1},
+{"sku":47,"price":"$74.91","product":"Cake Slab","Quantity":5},
+{"sku":48,"price":"$89.44","product":"Allspice - Jamaican","Quantity":18},
+{"sku":49,"price":"$34.23","product":"Cake - Night And Day Choclate","Quantity":21},
+{"sku":50,"price":"$86.36","product":"Sprouts - Baby Pea Tendrils","Quantity":21},
+{"sku":51,"price":"$8.35","product":"Olives - Black, Pitted","Quantity":8},
+{"sku":52,"price":"$13.94","product":"Rambutan","Quantity":12},
+{"sku":53,"price":"$93.78","product":"Cookie Trail Mix","Quantity":24},
+{"sku":54,"price":"$99.03","product":"Wine - White, Pinot Grigio","Quantity":15},
+{"sku":55,"price":"$13.23","product":"Chicken - Tenderloin","Quantity":16},
+{"sku":56,"price":"$90.04","product":"Cocktail Napkin Blue","Quantity":7},
+{"sku":57,"price":"$21.68","product":"Icecream - Dstk Super Cone","Quantity":17},
+{"sku":58,"price":"$73.10","product":"Oranges - Navel, 72","Quantity":16},
+{"sku":59,"price":"$68.42","product":"Garbage Bags - Black","Quantity":3},
+{"sku":60,"price":"$74.56","product":"Nougat - Paste / Cream","Quantity":15},
+{"sku":61,"price":"$75.91","product":"Flower - Leather Leaf Fern","Quantity":6},
+{"sku":62,"price":"$85.91","product":"Sandwich Wrap","Quantity":5},
+{"sku":63,"price":"$67.52","product":"Port - 74 Brights","Quantity":9},
+{"sku":64,"price":"$65.41","product":"Coconut - Creamed, Pure","Quantity":1},
+{"sku":65,"price":"$72.70","product":"Juice - Cranberry, 341 Ml","Quantity":2},
+{"sku":66,"price":"$83.60","product":"Lobster - Cooked","Quantity":14},
+{"sku":67,"price":"$98.56","product":"Pork - Bacon Cooked Slcd","Quantity":25},
+{"sku":68,"price":"$25.65","product":"Onion - Dried","Quantity":12},
+{"sku":69,"price":"$51.60","product":"Cranberries - Dry","Quantity":5},
+{"sku":70,"price":"$4.29","product":"Ecolab - Power Fusion","Quantity":15},
+{"sku":71,"price":"$11.72","product":"Sachet","Quantity":13},
+{"sku":72,"price":"$53.60","product":"Muffin Hinge Container 6","Quantity":5},
+{"sku":73,"price":"$35.31","product":"Mushroom - White Button","Quantity":13},
+{"sku":74,"price":"$73.78","product":"Coffee - Colombian, Portioned","Quantity":22},
+{"sku":75,"price":"$70.44","product":"Tomatoes - Orange","Quantity":3},
+{"sku":76,"price":"$3.31","product":"Pasta - Ravioli","Quantity":15},
+{"sku":77,"price":"$97.63","product":"Chicken - Leg, Boneless","Quantity":20},
+{"sku":78,"price":"$42.74","product":"Wiberg Super Cure","Quantity":8},
+{"sku":79,"price":"$92.29","product":"Beef - Ground Medium","Quantity":24},
+{"sku":80,"price":"$40.83","product":"Rice - Jasmine Sented","Quantity":4},
+{"sku":81,"price":"$51.03","product":"Apricots - Halves","Quantity":13},
+{"sku":82,"price":"$37.40","product":"Meldea Green Tea Liquor","Quantity":4},
+{"sku":83,"price":"$33.03","product":"Appetizer - Seafood Assortment","Quantity":7},
+{"sku":84,"price":"$54.96","product":"Radish","Quantity":9},
+{"sku":85,"price":"$71.68","product":"Pastry - Chocolate Marble Tea","Quantity":17},
+{"sku":86,"price":"$18.28","product":"Pepper - Chili Powder","Quantity":21},
+{"sku":87,"price":"$81.30","product":"Milk - Homo","Quantity":4},
+{"sku":88,"price":"$85.55","product":"Pesto - Primerba, Paste","Quantity":9},
+{"sku":89,"price":"$32.83","product":"Goldschalger","Quantity":21},
+{"sku":90,"price":"$23.92","product":"Tabasco Sauce, 2 Oz","Quantity":8},
+{"sku":91,"price":"$71.06","product":"Triple Sec - Mcguinness","Quantity":19},
+{"sku":92,"price":"$93.33","product":"Wine - Pinot Grigio Collavini","Quantity":1},
+{"sku":93,"price":"$30.37","product":"Tart - Lemon","Quantity":10},
+{"sku":94,"price":"$11.00","product":"Bread - Mini Hamburger Bun","Quantity":11},
+{"sku":95,"price":"$89.89","product":"Bread - Pumpernickle, Rounds","Quantity":15},
+{"sku":96,"price":"$44.24","product":"Tia Maria","Quantity":2},
+{"sku":97,"price":"$90.03","product":"Cookies - Englishbay Chochip","Quantity":25},
+{"sku":98,"price":"$47.59","product":"Tea - Lemon Green Tea","Quantity":5},
+{"sku":99,"price":"$11.63","product":"Hickory Smoke, Liquid","Quantity":10},
+{"sku":100,"price":"$70.68","product":"Tea - Herbal Sweet Dreams","Quantity":1}]
+
+let userList = [
+    { item: 'Jam - Apricot', quantity: 2 },
+    { item: 'Creamers - 10%', quantity: 1 },
+]
+
+function getTotalAmount(storeList,userList){
+
+   let totalAmount = 0; let currentQuantity = 0;
+
+    storeList.forEach(productDetail => {
+
+        userList.forEach(userDetail => {
+
+            if(productDetail.product === userDetail.item && 
+                (typeof (productDetail.product) === "string" && 
+                 typeof (userDetail.item) === "string")) {
+
+               if(userDetail.quantity > productDetail.Quantity){ 
+                currentQuantity = productDetail.Quantity;//multiply with store quantity alone
+                 //didn't consider the already existing item
+                productDetail.Quantity -= userDetail.quantity;    
+               }
+               else{
+                currentQuantity = userDetail.quantity;
+                //didn't consider the  already existing item
+                productDetail.Quantity -= userDetail.quantity;     
+
+               }
+               // removing special characters and converting the string to number
+               let price = productDetail.price.replace(/[$]/g,"");  
+               totalAmount += price * currentQuantity;
+            }
+            else
+            {
+                return `Invalid Inputs`;
+            }
+        })
+    })
+    return `$ ${totalAmount.toFixed(2)}`;
+}
+
+console.log(getTotalAmount(storeList,userList));
